fa78e8f12ff4715ab8e49bba8a21bd4413f617ef
Lwazi Mabota <greninja71@gmail.com>
2021-08-26 23:04:03 -0700

    Aligned grid in center, and added pixel trail when mouse enters the div

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const container = document.querySelector('.container') 
-const gridContainer = document.querySelector('#grid-container')
+const gridContainer = document.querySelector('#grid-container'); 
+
+function createDivs(col,rows) {
+     for(let i = 0; i < (col * rows); i++) { 
+        const element = document.createElement('div'); 
+        gridContainer.style.gridTemplateColumns = `repeat(${col},1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${rows},1fr)`; 
+        gridContainer.appendChild(element).classList.add('box') } 
+   
+   
+ } 
+    
+createDivs(16,16); 
 
 
-function createDivs(col,rows) { 
-    for(let i = 0; i < (col * rows); i++) { 
-    const element = document.createElement('div'); 
-    gridContainer.style.gridTemplateColumns = `repeat(${col},1fr)`; 
-    gridContainer.style.gridTemplateRows = `repeat(${rows},1fr)`;
-    gridContainer.appendChild(element).classList.add('box') } 
+ const box = document.querySelectorAll('.box'); 
+    box.forEach(function(box) { 
+    box.addEventListener('mouseenter',function() { 
+    this.style.backgroundColor ='#ff9999'; }); 
+}); 
 
-} 
-createDivs(16,16);
-
-//element.addEventListener("mouseover",event);//
-
-function randomColor () {
-
-}
+let eraseGrid = document.querySelector('#clear-grid');
