ab61e7b5ab345714f8490d0836ddc53dbfea19b2
Lwazi Mabota <greninja71@gmail.com>
2021-08-30 13:14:49 -0700

    Added random color button and reworked resize function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gridContainer = document.querySelector('#grid-container');
 const clearGrid = document.querySelector('#clear-grid');
 const resizeGrid = document.querySelector('#resize-button');
 const box = document.querySelectorAll('.box');
+const rgb = document.querySelector('#rgb');
 
 function createDivs(col, rows) {
 	gridContainer.innerHTML = '';
@@ -10,8 +11,8 @@ function createDivs(col, rows) {
 		const element = document.createElement('div');
 		gridContainer.style.gridTemplateColumns = `repeat(${col},1fr)`;
 		gridContainer.style.gridTemplateRows = `repeat(${rows},1fr)`;
-
 		gridContainer.appendChild(element).classList.add('box')
+
 		const box = document.querySelectorAll('.box');
 		box.forEach(function (box) {
 			box.addEventListener('mouseenter', function () {
@@ -28,11 +29,24 @@ function createDivs(col, rows) {
 
 		clearGrid.addEventListener('click', clearcontent);
 
-	}
+		function generateRandomColor() {
+			document.querySelectorAll('.box')
+			box.forEach(function (box) {
+				var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+				box.addEventListener('mouseenter', function () {
+					box.style.backgroundColor = randomColor;
+				return randomColor;
+				});
+			});
+
+			
+		}
+			rgb.addEventListener('click', generateRandomColor);
 
 
 }
 
+}
 createDivs(16, 16);
 
 
@@ -40,7 +54,7 @@ function resizecontent() {
 	document.querySelectorAll('.box');
 	box.forEach(function (box) {
 		box.style.backgroundColor = 'antiquewhite';
-		document.getElementById('grid-container').innerHTML = '';
+
 	});
 
 	let size = prompt("Please choose grid size between 5-64!", 16)
